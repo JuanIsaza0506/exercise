76fce15643acd8eabde0a3a90ad5f0980e777931
JuanIsaza0506 <juanisaza0506@gmail.com>
2024-07-31 16:17:29 -0500

    fix

---
+++ codes/index.js
@@ -0,0 +1,15 @@
+name: pipeline
+on:
+   push: 
+     branches;
+       - node 
+jobs: 
+    hello-word:
+     runs-on: ubuntu-latest
+     steps: 
+     - uses: actions/setup-node@v4
+       with:
+         node-version: 18
+
+      - name: hello word 
+        run: node codes\index.js
